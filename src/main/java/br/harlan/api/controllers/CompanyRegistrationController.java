f6ba8f774837814d39826156ab76a8a40b1b3352
Harlan Cleiton <harlancleiton@gmail.com>
2018-03-29 05:12:16 -0300

    Controller
    Criando controller para registro de empresas

--- src/main/java/br/harlan/api/controllers/CompanyRegistrationController.java
+++ src/main/java/br/harlan/api/controllers/CompanyRegistrationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.harlan.api.dtos.CompanyRegistrationDto;
 import br.harlan.api.entities.CompanyEntity;
 import br.harlan.api.entities.EmployeesEntity;
+import br.harlan.api.enums.ProfileEnum;
 import br.harlan.api.repositories.CompanyRepository;
 import br.harlan.api.repositories.EmployeesRepository;
 import br.harlan.api.response.Response;
 import br.harlan.api.services.CompanyService;
 import br.harlan.api.services.EmployeesService;
+import br.harlan.api.utils.PasswordUtil;
 
 @RestController
 @RequestMapping("/api/company-registration")
@@ -45,6 +48,14 @@ public class CompanyRegistrationController {
 	public CompanyRegistrationController() {
 	}
 
+	/**
+	 * 
+	 * @param companyRegistrationDto
+	 * @param bindingResult
+	 * @return ResponseEntity<Response<CompanyRegistrationDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
+	@PostMapping
 	public ResponseEntity<Response<CompanyRegistrationDto>> register(
 			@Valid @RequestBody CompanyRegistrationDto companyRegistrationDto, BindingResult bindingResult)
 			throws NoSuchAlgorithmException {
@@ -54,11 +65,28 @@ public class CompanyRegistrationController {
 		validate(companyRegistrationDto, bindingResult);
 
 		CompanyEntity companyEntity = dtoToCompanyEntity(companyRegistrationDto);
-		EmployeesEntity employeesEntity = dtoToEmployeesEmtity(companyRegistrationDto, bindingResult);
+		EmployeesEntity employeesEntity = dtoToEmployeesEntity(companyRegistrationDto, bindingResult);
+		
+		if(bindingResult.hasErrors()) {
+			LOGGER.error("Erro ao validar dados do Cadastro PJ: {}", bindingResult.getAllErrors());
+			bindingResult.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
 
-		return null;
+		companyService.create(companyEntity);
+		employeesEntity.setCompanyEntity(companyEntity);
+		employeesService.create(employeesEntity);
+		
+		response.setData();
+		return response;
 	}
 
+	/**
+	 * Verifica se já existe no banco de dados o CNPJ ou CPF informado
+	 * 
+	 * @param companyRegistrationDto
+	 * @param bindingResult
+	 */
 	private void validate(CompanyRegistrationDto companyRegistrationDto, BindingResult bindingResult) {
 		companyService.findByCnpj(companyRegistrationDto.getCnpj())
 				.ifPresent(company -> bindingResult.addError(new ObjectError("Empresa", "Empresa já existe")));
@@ -69,14 +97,53 @@ public class CompanyRegistrationController {
 
 	}
 
+	/**
+	 * Converte os dados do DTO para CompanyEntity
+	 * 
+	 * @param companyRegistrationDto
+	 * @return CompanyEntity
+	 */
 	private CompanyEntity dtoToCompanyEntity(CompanyRegistrationDto companyRegistrationDto) {
-		// TODO Auto-generated method stub
-		return null;
+		CompanyEntity companyEntity = new CompanyEntity();
+		companyEntity.setCnpj(companyRegistrationDto.getCnpj());
+		companyEntity.setSocialName(companyRegistrationDto.getSocialName());
+		return companyEntity;
 	}
 
-	private EmployeesEntity dtoToEmployeesEmtity(CompanyRegistrationDto companyRegistrationDto,
-			BindingResult bindingResult) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Converte os dados do DTO para EmployeeEntity
+	 * 
+	 * @param companyRegistrationDto
+	 * @param bindingResult
+	 * @return employeesEntity
+	 * @throws NoSuchAlgorithmException
+	 */
+	private EmployeesEntity dtoToEmployeesEntity(CompanyRegistrationDto companyRegistrationDto,
+			BindingResult bindingResult) throws NoSuchAlgorithmException {
+		EmployeesEntity employeesEntity = new EmployeesEntity();
+		employeesEntity.setName(companyRegistrationDto.getName());
+		employeesEntity.setEmail(companyRegistrationDto.getEmail());
+		employeesEntity.setCpf(companyRegistrationDto.getCpf());
+		employeesEntity.setProfileEnum(ProfileEnum.ROLE_ADMIN);
+		employeesEntity.setPassword(PasswordUtil.generateBCrypt(companyRegistrationDto.getPassword()));
+		return employeesEntity;
+	}
+	
+	/**
+	 * Popula o DTO de cadastro com os dados do funcionário e empresa
+	 * 
+	 * @param employeesEntity
+	 * @return companyRegistrationDto
+	 */
+	private CompanyRegistrationDto entityToDto(EmployeesEntity employeesEntity) {
+		CompanyRegistrationDto companyRegistrationDto = new CompanyRegistrationDto();
+		companyRegistrationDto.setId(employeesEntity.getId());
+		companyRegistrationDto.setName(employeesEntity.getName());
+		companyRegistrationDto.setEmail(employeesEntity.getEmail());
+		companyRegistrationDto.setCpf(employeesEntity.getCpf());
+		companyRegistrationDto.setCnpj(employeesEntity.cnpj);
+		companyRegistrationDto.setCnpj(employeesEntity.getCompanyEntity().getCnpj()());
+		companyRegistrationDto.setSocialName(employeesEntity.getCompanyEntity().getSocialName());
+		return companyRegistrationDto;
 	}
 }
